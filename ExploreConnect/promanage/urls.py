129bc9ce309fb0e68071b8cf60c8cb2ea6457e15
LeenaVelayudhan <leenavelayudhan93@gmail.com>
2024-09-27 11:53:54 +0530

    Update urls.py

--- ExploreConnect/promanage/urls.py
+++ ExploreConnect/promanage/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('language_translation/', views.language_translation, name='language_translation'),
-    path('language_translation/translate_audio/', views.translate_audio, name='translate_audio'), 
+    path('manageprofile/', views.manageprofile, name='manageprofile'),
+
 ]
 
