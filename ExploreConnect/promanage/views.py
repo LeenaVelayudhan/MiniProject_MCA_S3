70bb985ca48fe925f29a3aa7ee93777d33a18f95
LeenaVelayudhan <leenavelayudhan93@gmail.com>
2024-09-27 11:51:59 +0530

    Update views.py

--- ExploreConnect/promanage/views.py
+++ ExploreConnect/promanage/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+def manageprofile(request):
+    user= user.objects.get(email=request.user.email)  # Assuming email is unique
 
-# Create your views here.
+    if request.method == 'POST':
+      
+        user.name = request.POST['username']
+        user.email = request.POST['email']
+        user.password=request.POST['password'] 
+
+       
+        user.save()
+
+       
+        messages.success(request, 'Profile updated successfully!')
+        return redirect('manageprofile')
+
+    return render(request, 'manageprofile.html', {'user': user})
